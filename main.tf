b9c56a071f00f6938ceaee5e3de646c421473cd3
mohammadikram01 <mdikram0024@gmail.com>
2025-08-10 11:07:47 +0530

    commited all

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 2.13.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "npipe:////./pipe/docker_engine"
+}
+
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  name  = "tutorial-nginx"
+  image = docker_image.nginx.name
+
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
